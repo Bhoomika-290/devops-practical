632512c2b70971b3a10312f189abb4d9d1a9f03d
Bhoomika-290 <sbhoomika295@gmail.com>
2026-04-09 12:40:34 +0530

    Changes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)   # ✅ MUST be before routes
+
+@app.route("/")
+def home():
+    return "Hello! DevOps Practical Running Successfully"
+
 @app.route("/login/<username>/<password>")
 def login(username, password):
     if username == "admin" and password == "1234":
         return "Login Successful"
     else:
         return "Invalid Credentials"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
